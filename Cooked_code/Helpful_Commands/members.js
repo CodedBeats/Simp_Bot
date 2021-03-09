2f07cb5dc73f7789b664bda4bf0edf1290dc423d
Luca Haar <luca.haar@icloud.com>
2021-03-09 19:00:48 +1100

    added a 'member count' and 'bot's servers' function

---
+++ Cooked_code/Helpful_Commands/members.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+
+module.exports = client => {
+    client.on("message", message => {
+        if (message.content.toLowerCase() === "$member count") {
+            let serverID = message.guild.id;
+            let guild = client.guilds.cache.get(serverID)
+            let members = guild.memberCount // - 5
+            // message.channel.send(`The ${message.guild.name} has ${members} members`)
+            console.log("Server member count")
+
+            const embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle("Server's Member", '')
+            .setDescription(`The ${message.guild.name} has ${members} members`)
+            message.channel.send(embed);
+        }
+    })
+
+
+    client.on("message", message => {
+        if (message.content.toLowerCase() === "$bot servers") {
+            
+            let serverlist = ''
+            client.guilds.cache.forEach((guild) => {
+                serverlist = serverlist.concat(" - " + guild.name + ": Members: " + guild.memberCount + "\n")
+            })
+    
+            const embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle("Servers With Project Continuum", '')
+            .setDescription(serverlist)
+            message.channel.send(embed);
+        }
+    })
+
+}
